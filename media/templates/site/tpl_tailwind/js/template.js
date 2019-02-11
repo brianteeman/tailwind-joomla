27834c03f81fed30a62a2cc3a934c9f7dc5235de
dGrammatiko <d.grammatiko@gmail.com>
2019-02-11 20:45:36 +0100

    js tweaks

--- media/templates/site/tpl_tailwind/js/template.js
+++ media/templates/site/tpl_tailwind/js/template.js
@@ -1,68 +1,74 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	function trackScroll() {
-		var scrolled = window.pageYOffset;
-		var coords = document.documentElement.clientHeight;
+  function trackScroll() {
+    var goTopBtn = document.getElementById('back-to-top');
 
-		var goTopBtn = document.getElementById('back-to-top');
+    if (goTopBtn) {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
 
-		if (typeof goTopBtn === "undefined") {
-			return;
-		}
+      if (scrolled > coords) {
+        goTopBtn.classList.remove('hidden');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.add('hidden');
+      }
+    }
+  }
 
-		if (scrolled > coords) {
-			goTopBtn.classList.remove('hidden');
-		}
-		if (scrolled < coords) {
-			goTopBtn.classList.add('hidden');
-		}
-	}
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
 
-	function backToTop() {
-		if (window.pageYOffset > 0) {
-			window.scrollBy(0, -80);
-			setTimeout(backToTop, 0);
-		}
-	}
+  function closeAlert(event) {
+    event.target.removeEventListener('click', this)
+    var parent = event.target.parentNode;
 
-	document.addEventListener("DOMContentLoaded", function() {
-		var goTopBtn = document.getElementById('back-to-top');
+    if (parent) {
+      parent.parentNode.removeChild(parent);
+    }
+  }
 
-		if (goTopBtn) {
-			window.addEventListener('scroll', trackScroll);
-			goTopBtn.addEventListener('click', backToTop);
-		}
+  function navToggle(event) {
+    // Get the "main-nav" element
+    var $target = document.getElementById('main-nav');
 
-		var alertCloseButtons = document.querySelectorAll("[data-dismiss='tailalert']");
+    // Toggle the class on "main-nav"
+    $target.classList.toggle('hidden');
+  }
 
-		for (var i = 0, len = alertCloseButtons.length; i < len; i++) {
-			alertCloseButtons[i].addEventListener('click', function(){
-				var parent = this.parentNode;
-				
-				if (parent) {
-					parent.parentNode.removeChild(parent);
-				}
-			});
-		}
+  function init() {
+    // Let GC remove unneeded parts
+    document.removeEventListener('DOMContentLoaded', init);
 
-		// Get all "navbar-burger" elements
-		var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    var goTopBtn = document.getElementById('back-to-top');
 
-		// Check if there are any navbar burgers
-		if ($navbarBurgers.length > 0) {
-			// Add a click event on each of them
-			$navbarBurgers.forEach(function ($el) {
-				$el.addEventListener('click', function () {
+    if (goTopBtn) {
+      window.addEventListener('scroll', trackScroll);
+      goTopBtn.addEventListener('click', backToTop);
+    }
 
-				// Get the "main-nav" element
-				var $target = document.getElementById('main-nav');
+    var alertCloseButtons = document.querySelectorAll("[data-dismiss='tailalert']");
 
-				// Toggle the class on "main-nav"
-				$target.classList.toggle('hidden');
+    for (var i = 0, len = alertCloseButtons.length; i < len; i++) {
+      alertCloseButtons[i].addEventListener('click', closeAlert);
+    }
 
-				});
-			});
-		}
-	});
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function($el) {
+        $el.addEventListener('click', navToggle);
+      });
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", init);
 })();
