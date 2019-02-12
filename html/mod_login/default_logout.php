de5a2438c0d8e0f924c6e9d8a7f4a6f1cbd46ebe
Pablo Arias <correo@pabloarias.eu>
2019-02-13 00:58:57 +0100

    Login module override (logout tmpl)

---
+++ html/mod_login/default_logout.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_login
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+?>
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure', 0)); ?>" method="post" id="login-form">
+<?php if ($params->get('greeting', 1)) : ?>
+	<div class="login-greeting my-2">
+	<?php if (!$params->get('name', 0)) : ?>
+		<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('name'), ENT_COMPAT, 'UTF-8')); ?>
+	<?php else : ?>
+		<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('username'), ENT_COMPAT, 'UTF-8')); ?>
+	<?php endif; ?>
+	</div>
+<?php endif; ?>
+<?php if ($params->get('profilelink', 0)) : ?>
+	<div class="my-2">
+			<a href="<?php echo JRoute::_('index.php?option=com_users&view=profile'); ?>">
+			<?php echo JText::_('MOD_LOGIN_PROFILE'); ?></a>
+	</div>
+<?php endif; ?>
+	<div class="logout-button">
+		<input type="submit" name="Submit" class="bg-grey-light hover:bg-grey text-grey-darkest py-2 px-4 my-2 rounded cursor-pointer" value="<?php echo JText::_('JLOGOUT'); ?>" />
+		<input type="hidden" name="option" value="com_users" />
+		<input type="hidden" name="task" value="user.logout" />
+		<input type="hidden" name="return" value="<?php echo $return; ?>" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
